8c511555e0e8698d6b46e66b573e959eaa966e75
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2020-10-07 19:47:37 +0200

    Add a function to MainActivity to show a Toast

--- app/src/main/java/com/davidmendozamartinez/myplayer/MainActivity.kt
+++ app/src/main/java/com/davidmendozamartinez/myplayer/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.davidmendozamartinez.myplayer
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toast("Hello World")
+    }
+
+    private fun toast(text: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this, text, duration).show()
     }
 }
