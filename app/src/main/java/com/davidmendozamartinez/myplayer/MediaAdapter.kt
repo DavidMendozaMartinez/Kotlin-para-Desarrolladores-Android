1ef5997abeca73b8036288b372b7280a3f9ed6e3
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2020-10-11 16:55:35 +0200

    Add a video indicator to the MediaItem item layout. Show video indicator when MediaItem type is Video

--- app/src/main/java/com/davidmendozamartinez/myplayer/MediaAdapter.kt
+++ app/src/main/java/com/davidmendozamartinez/myplayer/MediaAdapter.kt
@@ -24,10 +24,15 @@ class MediaAdapter(private val items: List<MediaItem>) : RecyclerView.Adapter<Me
 
         private val title: TextView = view.findViewById(R.id.mediaTitle)
         private val thumb: ImageView = view.findViewById(R.id.mediaThumb)
+        private val videoIndicator: ImageView = view.findViewById(R.id.mediaVideoIndicator)
 
         fun bind(mediaItem: MediaItem) {
             title.text = mediaItem.title
             thumb.loadUrl(mediaItem.url)
+            videoIndicator.visibility = when (mediaItem.type) {
+                MediaItem.Type.PHOTO -> View.GONE
+                MediaItem.Type.VIDEO -> View.VISIBLE
+            }
 
             itemView.setOnClickListener { toast(mediaItem.title) }
         }
